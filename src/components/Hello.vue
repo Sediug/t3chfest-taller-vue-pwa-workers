ca3e6cdb4dbf6c173b055d7018ba86f24ba19b38
sebastiancbvz <sebastian.cabanas@cbiconsulting.es>
2018-03-02 13:22:36 +0100

    Fix firebase, changes in css

--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -2,8 +2,8 @@
   <div class="hello">
     <ul>
       <li v-for="contact in contacts" :key="contact">
-        {{contact.name}}
-        {{contact.email}}
+        <div class="name">{{contact.name}}<div>
+        <div class="email">{{contact.email}}<div>
       </li>
     </ul>
   </div>
@@ -40,6 +40,19 @@ ul {
 li {
   display: inline-block;
   margin: 0 10px;
+  text-align: center;
+}
+
+li.name {
+  color: #444;
+  display: block;
+  font-size: 20px;
+}
+
+li.name {
+  color: rgb(180, 152, 25);
+  display: block;
+  font-size: 12px;
 }
 
 a {
